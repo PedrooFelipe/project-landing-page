a5fbfe7592d21b4943851285e84cfd7a619a350e
Pedro Felipe <pedro234felipe@gmail.com>
2025-12-04 10:53:22 -0300

    added the js file to create the functionfor the arrow in the painel

---
+++ src/js/painel.js
@@ -0,0 +1,34 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+function esconderImagem(){
+    imagensPainel.forEach(imagem =>{
+        imagem.classList.remove('mostrar');
+    });
+};
+function mostrarImagem(){
+    imagensPainel[imagemAtual].classList.add('mostrar');
+};
+
+setaAvancar.addEventListener('click', function(){
+    const totalImagens = imagensPainel.length - 1;
+    if (imagemAtual === totalImagens){
+        return;
+    }
+
+    imagemAtual++;
+    esconderImagem();
+    mostrarImagem();
+});
+setaVoltar.addEventListener('click', function(){
+    if (imagemAtual === 0){
+        return;
+    }
+
+    imagemAtual--;
+
+    esconderImagem();
+    mostrarImagem();
+});
